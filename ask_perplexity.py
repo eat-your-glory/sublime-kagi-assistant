251b8e40bc2a8ccd621c42c6f9c5fb2f5f545a9f
Spencer Christensen <spencer@thismoment.com>
2025-02-05 21:32:36 -0600

    update for py3.3 and make focus easier to use

--- ask_perplexity.py
+++ ask_perplexity.py
@@ -3,51 +3,56 @@ import sublime_plugin
 import webbrowser
 from urllib.parse import quote
 
+FOCUS_OPTIONS = {
+    "Web": "web",
+    "Academic": "academic",
+    "Writing": "writing",
+    "Math": "math",
+}
+
 
 class AskPerplexityCommand(sublime_plugin.WindowCommand):
     def run(self, focus, query):
-        url = f"https://www.perplexity.ai/search?q={quote(query)}&focus={focus}"
+        url = "https://www.perplexity.ai/search?q={}&focus={}".format(
+            quote(query), focus
+        )
         webbrowser.open(url)
-    
+
     def input(self, args):
-        if "query" in args:
-            return None
-
-        if "focus" in args:
-            return QueryInputHandler()
-
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        default_focus = settings.get("perplexity_focus")
-
-        if default_focus:
-            args["focus"] = default_focus
-            return QueryInputHandler()
-
         return FocusInputHandler()
 
 
 class FocusInputHandler(sublime_plugin.ListInputHandler):
-    def name(self):
-        return "focus"
+    def __init__(self):
+        super().__init__()
+        self.last_focus = None
 
     def placeholder(self):
         return "Select focus"
 
+    def initial_text(self):
+        if self.last_focus:
+            return self.last_focus
+
+        # TODO: add settings for default option
+        # settings = sublime.load_settings("AskPerplexity.sublime-settings")
+        # default_focus = settings.get("perplexity_focus")
+        # if default_focus in FOCUS_OPTIONS.values():
+        #     return default_focus
+
+    def confirm(self, focus):
+        self.last_focus = focus
+        return focus
+
     def list_items(self):
-        return [
-            ("Web", "web"),
-            ("Academic", "academic"),
-            ("Writing", "writing"),
-            ("Math", "math"),
-        ]
+        order = ["Web", "Academic", "Math", "Writing"]
+        ordered_items = [(key, FOCUS_OPTIONS[key]) for key in order]
+        return list(ordered_items)
 
     def next_input(self, args):
         return QueryInputHandler()
 
 
 class QueryInputHandler(sublime_plugin.TextInputHandler):
-    def name(self):
-        return "query"
-
     def placeholder(self):
         return "Enter your question"
